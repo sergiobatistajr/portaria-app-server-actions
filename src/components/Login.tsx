626b49cb97785133a78f26aad302269c9ba121ab
surfacez <sergiobatistajr94@gmail.com>
2023-09-25 14:47:21 -0300

    login component

---
+++ src/components/Login.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const schema = z.object({
+  username: z
+    .string()
+    .min(3, {
+      message: "Usuário deve ter no mínimo 3 caracteres",
+    })
+    .max(20, {
+      message: "Usuário deve ter no máximo 20 caracteres",
+    }),
+  password: z
+    .string()
+    .min(6, {
+      message: "Senha deve ter no mínimo 6 caracteres",
+    })
+    .max(20, {
+      message: "Senha deve ter no máximo 20 caracteres",
+    }),
+});
+
+export default function Login() {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof schema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form>
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Usuário</FormLabel>
+              <FormControl>
+                <Input type="text" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Senha</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div>
+          <Button type="submit">Entrar</Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
